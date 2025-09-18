2706042d1e0063b30c17cbe75a56b2bd13e16402
menu <aleguiroc@alu.edu.gva.es>
2025-09-18 14:42:36 +0200

    metodo crear directorio

--- main.java
+++ main.java
@@ -1,10 +1,17 @@
 import java.io.*;
 import java.util.*;
+import java.io.File;
 
 
 
-
-
+public class CrearDirectorio {
+        File nuevoDirectorio = new File("MiNuevaCarpeta");
+        if (nuevoDirectorio.mkdir()) {
+            System.out.println("Directorio creado: " + nuevoDirectorio.getName());
+        } else {
+            System.out.println("El directorio ya existe o no se pudo crear.");
+        }
+}
 
 
 public class Main {
