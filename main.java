98c4ae2cecadabe065ccc6b6325defc439c1bd12
menu <aleguiroc@alu.edu.gva.es>
2025-09-18 14:40:15 +0200

    menu

--- main.java
+++ main.java
@@ -1,8 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+        }
     }
 }
